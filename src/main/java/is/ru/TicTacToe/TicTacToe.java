875072036b8ce66950765ed7220a3cff935a08da
root <root@MEGATRON.lan>
2016-10-23 23:27:00 +0000

    Gradle lagfæringar og reyna fá til að virka á Skelinni hjá öllum

---
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -0,0 +1,8 @@
+package is.ru.TicTacToe;
+
+public class TicTacToe {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
+
---
+++ src/test/java/is/ru/TicTacToe/TicTacToeTest.java
@@ -0,0 +1,12 @@
+package is.ru.TicTacToe;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+	@Test
+	public void Test() {
+		assertEquals("Hello", "Hello");
+	}
+
+}
