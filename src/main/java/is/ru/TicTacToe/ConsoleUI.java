43f3071759adf27f136e9e44169b4caec077ebcf
Andri Guðmundsson <andri@i-1217c9a8.cs1004cloud.internal>
2016-10-26 16:23:30 +0000

    ConsoleUI and ConsoleUITest added

---
+++ src/main/java/is/ru/TicTacToe/ConsoleUI.java
@@ -0,0 +1,26 @@
+package is.ru.TicTacToe;
+
+import java.util.Scanner;
+
+public class ConsoleUI{
+    private Board board;
+    private Scanner sc;
+
+    ConsoleUI()
+    {
+	board = new Board();
+	sc = new Scanner(System.in);
+    }
+
+    public int getInt(String str)
+    {
+	try
+	{
+	    return Integer.parseInt(str);
+	}
+	catch(NumberFormatException e)
+	{
+	    return -1;
+	}
+    }
+}
