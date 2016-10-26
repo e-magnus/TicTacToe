bba9015b5858ed61e06852af69176b07bf4bf182
Svanur Jóhannesson <svanurjoh@i-1217c9a8.cs1004cloud.internal>
2016-10-26 16:19:34 +0000

    make og print board

--- src/test/java/is/ru/TicTacToe/BoardTest.java
+++ src/test/java/is/ru/TicTacToe/BoardTest.java
@@ -12,4 +12,22 @@ public class BoardTest {
 		b.isValidMove(3);
 		b.isValidMove(8);
 	}
+	
+	@Test (expected = IndexOutOfBoundsException.class)
+    public void testInputTooLow() {
+        Board b = new Board();
+        b.validateIndex(-1);
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void testInputTooHigh() {
+        Board b = new Board();
+        b.validateIndex(9);
+    }
+	
+	@Test
+    public void testToString() {
+        Board board = new Board();
+        assertEquals("   |   |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   \n", board.printBoard());
+    }
 }
