66e4cb40f6b0137e5a40d9f0c14a6854e4266096
Bergvin Gíslason <bergvin15@i-1217c9a8.cs1004cloud.internal>
2016-10-26 22:24:40 +0000

    ConsoleUI test

---
+++ src/test/java/is/ru/TicTacToe/ConsoleUITest.java
@@ -0,0 +1,17 @@
+package is.ru.TicTacToe;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ConsoleUITest {
+    @Test
+    public void testGetInt() {
+		/*
+        ConsoleUI ui = new ConsoleUI();
+        assertEquals(-1, ui.getInt("asdf"));
+        assertEquals(-1, ui.getInt("50asdf"));
+        assertEquals(-1, ui.getInt(""));
+        assertEquals(1, ui.getInt("1"));
+        assertEquals(50, ui.getInt("50"));*/
+    }
+}
+
