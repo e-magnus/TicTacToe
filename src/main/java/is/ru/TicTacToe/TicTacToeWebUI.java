940d7ef2c6e2620688b06f1b89eee77dccac8912
Gardar Mar <gardarmar@i-1217c9a8.cs1004cloud.internal>
2016-10-26 00:36:10 +0000

    Bæta við index.html og reyna fá eh á skjáinn í heroku appinu

--- src/main/java/is/ru/TicTacToe/TicTacToeWebUI.java
+++ src/main/java/is/ru/TicTacToe/TicTacToeWebUI.java
@@ -3,7 +3,26 @@ package is.ru.tictactoe;
 import static spark.Spark.*;
  
 public class TicTacToeWebUI {
-    public static void main(String[] args) {
-        get("/", (req, res) -> "Hello, World!");
+	private static Board board;
+	
+    public static void setup() {
+		board = new Board();
+		
+		staticFileLocation("/public");
+        SparkApplication tttweb = new TicTacToeWebUI();
+		
+		String port = System.getenv("PORT");
+        if (port != null) {
+            port(Integer.valueOf(port));
+        }
+		ticweb.init();
+	}
+	
+	@Override
+    public void init() {
+        post("/Initialize", (req, res) -> {
+            board.initializeBoard();
+            return "";
+        });
     }
 }
