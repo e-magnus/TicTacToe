7d3d333da01e92ada92c9a1e31a92718442f94fb
Magnús Einarsson <magnus@i-1217c9a8.cs1004cloud.internal>
2016-10-27 15:10:49 +0000

    Comments added for Player.java

--- src/main/java/is/ru/TicTacToe/Player.java
+++ src/main/java/is/ru/TicTacToe/Player.java
@@ -1,30 +1,36 @@
 package is.TicTacToe;
 
 public class Player {
-    private String nickname;
-	private String symbol;
+
+    private String nickname;	// Players nick name.
+	private String symbol;		// Players symbol like "X" or "O".
 	
+	// Constructor for player, takes nickname and symbol for player as arguments.
 	Player(String nick, String symb)
 	{
 		setNickname(nick);
 		setSymbol(symb);
 	}
 	
+	// Returns players nickname.
 	public String getNickname()
 	{
 		return nickname;
 	}
 	
+	// Returns playersd symbol.
 	public String getSymbol()
 	{
 		return symbol;
 	}
 	
+	// Sets players nickname.
 	public void setNickname(String nick)
 	{
 		nickname = nick;
 	}
 	
+	// Set players symbol.
 	public void setSymbol(String symb)
 	{
 		symbol = symb.substring(0, 1);
