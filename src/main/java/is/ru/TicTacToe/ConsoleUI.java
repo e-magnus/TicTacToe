cfa1de96d69e59cd00e8685fc05292c881fb814b
Gardar Mar <gardarmar@i-1217c9a8.cs1004cloud.internal>
2016-10-27 00:05:09 +0000

    laga indent í ConsoleUI

--- src/main/java/is/ru/TicTacToe/ConsoleUI.java
+++ src/main/java/is/ru/TicTacToe/ConsoleUI.java
@@ -8,19 +8,19 @@ public class ConsoleUI{
 
     ConsoleUI()
     {
-	board = new Board();
-	sc = new Scanner(System.in);
+		board = new Board();
+		sc = new Scanner(System.in);
     }
 
     public int getInt(String str)
     {
-	try
-	{
-	    return Integer.parseInt(str);
-	}
-	catch(NumberFormatException e)
-	{
-	    return -1;
-	}
+		try
+		{
+			return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e)
+		{
+			return -1;
+		}
     }
 }
