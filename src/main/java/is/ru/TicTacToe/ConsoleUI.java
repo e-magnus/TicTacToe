a9c296d5c2ff4ca92c5a09197fe6601ff6c4814e
Andri Guðmundsson <andri@i-1217c9a8.cs1004cloud.internal>
2016-10-28 14:15:17 +0000

    laga ConsoleUI

--- src/main/java/is/ru/TicTacToe/ConsoleUI.java
+++ src/main/java/is/ru/TicTacToe/ConsoleUI.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class ConsoleUI{
     private Board board;
     private Scanner sc;
+    private Player p1, p2, curr;
 
     ConsoleUI()
     {
@@ -23,5 +24,63 @@ public class ConsoleUI{
 		{
 			return -1;
 		}
+    }
+
+    public void initializeGame()
+    {
+        System.out.println("Enter name for player 1: ");
+        p1 = new Player(sc.nextLine(), "X");
+       
+        System.out.println("Enter name for player 2: ");
+        p2 = new Player(sc.nextLine(), "O");
+    }
+   
+    public void switchPlayer()
+    {
+        if(curr == p1)
+        {
+            curr = p2;
+        }
+        else
+        {
+            curr = p1;
+        }
+    }
+   
+    public void startGame() {
+        
+        initializeGame();
+        curr = p1;
+ 
+        System.out.println(p1.getNickname() + " vs. " + p2.getNickname());
+        System.out.println("------------------------------");
+        while (true) {
+            System.out.println("It's your turn " + curr.getNickname());
+            System.out.println(board);
+            System.out.print("Select cell from 1 to 9: ");
+            int move = getInt(sc.nextLine());
+            if (move < 1 || move > 9) {
+                System.out.println("Invalid input. Please try again.");
+                continue;
+            } else if (!board.isEmpty(move - 1)) {
+                System.out.println("Cell number " + move + " is not empty!. Try again.");
+                continue;
+            }
+ 
+            board.insertSymbol(curr, move - 1);
+ 
+            if (board.isWinner(curr)) {
+                System.out.println(board);
+                System.out.println("Winner is: " + curr.getNickname());
+                break;
+            } else if (board.isDraw()) {
+                System.out.println(board);
+                System.out.println("Draw!");
+                break;
+            }
+ 
+            switchPlayer();
+            System.out.println();
+        }
     }
 }
